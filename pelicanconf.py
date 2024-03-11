0e5e5d1e1d601dfba3e33af04784c3725c323296
Tim Gilbert <timgilbert@gmail.com>
2024-03-11 11:45:52 -0400

    More tweaking

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,7 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None

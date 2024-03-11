7607d0b80a1df3598b64e922e6f23931c0d2236e
timgilbert <timgilbert@gmail.com>
2024-03-11 20:26:48 +0000

    Update rendered to output generated at b98b2b3

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -16,10 +16,4 @@ var store = [{
         "tags": ["bread","sourdough"],
         "url": "/breadlog/2024/02/26/pain-au-levain.html",
         "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2024/03/11/welcome-to-jekyll.html",
-        "teaser": null
       }]

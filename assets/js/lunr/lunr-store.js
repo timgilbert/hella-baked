ed88086a7f117b9c0d920753ee5b977306b5e0b9
timgilbert <timgilbert@gmail.com>
2024-03-11 19:56:34 +0000

    Update rendered to output generated at c147cb4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Beginner's Sourdough Bread",
+        "excerpt":"Beginner’s Sourdough Bread https://www.theperfectloaf.com/beginners-sourdough-bread/ making 1 loaf, 1/2 formula Levain, 9:40am: 19g starter 19g wheat 19g bread 38g water Total: 61g !?, 9:41am Seems like I lost some mass somewhere? ready for mixing 2:40 - 3:40 Autolyse, 1:40pm: 387g bread 57g whole wheat 26g dark rye 304g water Save 25g...","categories": ["breadlog"],
+        "tags": ["bread","sourdough","failure"],
+        "url": "/breadlog/2024/02/21/beginners-sourdough-bread.html",
+        "teaser": null
+      },{
+        "title": "Beginner's Sourdough Bread, Take 2",
+        "excerpt":"Schedule https://www.theperfectloaf.com/beginners-sourdough-bread/#h-baking-schedule 11am levain 5-6 hours 3pm autolyse 1 hour 4pm Mix 3 folds at 30 mins: 4:30, 5:00, 5:30 8:15pm divide, preshape, bench rest Bench rest 25 min 8:35pm shape 8:40pm proof on counter 9pm retard overnight 12:30pm bake Weights small metal ramekin 34g big mixing bowl 344g med...","categories": ["breadlog"],
+        "tags": ["bread","sourdough"],
+        "url": "/breadlog/2024/02/23/sourdough-take-2.html",
+        "teaser": null
+      },{
+        "title": "Pain au Levain",
+        "excerpt":"https://www.kingarthurbaking.com/recipes/pain-au-levain-recipe Going to try making a batard here, also making the full recipe for 2 loaves Levain My starter was slightly tired-looking, hope everything is cool Surprisingly stiff Started at 4:30pm. For a 12 hour ferment, that’s not a great schedule. May refrigerate for a few hours to slow it...","categories": ["breadlog"],
+        "tags": ["bread","sourdough"],
+        "url": "/breadlog/2024/02/26/pain-au-levain.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2024/03/11/welcome-to-jekyll.html",
+        "teaser": null
+      }]
